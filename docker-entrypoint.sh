60982c6f8e2429669d81a8b31bce5cdac30cdb46
Fabio Tea <iam@f4b.io>
2021-08-02 15:09:17 +0200

    added custom entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ ! test -f /app/.runner ]; then
+if [ ! -f /app/.runner ]; then
   /app/config.sh --unattended --url $REPOSITORY_URL --token $REPOSITORY_TOKEN --replace --labels $RUNNER_RUNS_ON
 fi
 
